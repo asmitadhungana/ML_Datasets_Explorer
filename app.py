87ba9c7114285a0753cac94d7a7f4301dc0db65b
Asmee Dhungana <dhungana.asmee99@gmail.com>
2020-08-15 18:50:14 +0545

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import seaborn as sns
+import os
+import streamlit as st
+
+# EDA pkgs
+import pandas as pd
+
+# Visualization pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
+
+
+def main():
+    """Common ML Dataset Explorer"""
+    st.title("Common ML Dataset Explorer")
+    st.subheader("Simple Data Science Explorer with Streamlit")
+
+
+if __name__ == "__main__":
+    main()
